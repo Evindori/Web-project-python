fb38599e91a352ae1922e292cc08d7f046dafe79
Evindori <evindori@gmail.com>
2020-05-18 13:20:00 +0300

    всё ещё не работает, но уже как-то более осмысленно

--- app/test.py
+++ app/test.py
@@ -1,13 +1,79 @@
-from __init__ import app
-from routes import reveresed, create_file, getnmbr, getdata, getstat
+# -*- coding: utf-8 -*-
 import unittest
+import requests
+from flask import render_template
+from collections import namedtuple
+import codecs
+import sys
 import os
+
+sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
+def create_file(web, address):
+  response = requests.get(web)
+  with open(address, 'wb') as output_file:
+    output_file.write(response.content)
+
+
+def getnmbr(string):
+    ans = 0
+    for i in string:
+        if i.isdigit():
+            ans = ans*10 + int(i)
+    return ans
+
+
+def getdata(address, web='http://coronavirus-monitor.ru/coronavirus-v-moskve/'):
+    create_file(web, address)
+
+    with open(address, 'r', encoding='utf-8') as input_file:
+        data_disease = 0
+        data_healed = 0
+        data_deathes = 0
+        fincome = 1
+        for line in input_file:
+            if line.find("Заражений") != -1 and fincome:
+                data_disease = getnmbr(line)
+            if line.find("Выздоровлений") != -1 and fincome:
+                data_healed = getnmbr(line)
+            if line.find("Смертей") != -1 and fincome:
+                data_deathes = getnmbr(line)
+                fincome = 0
+    return {'data_disease': str(data_disease),
+            'data_healed': str(data_healed),
+            'data_deathes': str(data_deathes)}
+
+
+def getstat(address, web='https://coronavirusstat.ru/country/moskva/263672/'):
+    create_file(web, address)
+    data = {}
+    with open(address, 'r', encoding='utf-8') as input_file:
+            line = input_file.readline()
+            while line != '</html>':
+                line = input_file.readline()
+                if line.find("Случаев</th>") != -1:
+                    n = 9
+                    while line.find('</tbody>') == -1:
+                        while line.find("<th>") == -1:
+                            line = input_file.readline()
+                        date = line.strip()
+                        clis = []
+                        clis.append(str(date[4:-7]))
+                        while line.find('</tr>') == -1:
+                            line = input_file.readline()
+                            if line.find('<td>') != -1:
+                                line = input_file.readline()
+                                clis.append(getnmbr(line))
+                        data.update({n : clis})
+                        n -= 1
+                        line = input_file.readline()
+                        line = input_file.readline()
+    return reveresed(data)
 
 
 class MyTestCase(unittest.TestCase):
     def test_create_file(self):
         web = 'file:///' + os.path.join(os.path.dirname(os.path.abspath('test0.html')), 'test0.html')
-        create_file(web, 'anstest0.html')
+        create_file(web.replace("\\", "/"), 'anstest0.html')
         with open('test0.html', 'r') as t:
             with open('anstest0.html', 'r') as at:
                 self.assertEqual(at.read(), t.read())
@@ -17,16 +83,16 @@ class MyTestCase(unittest.TestCase):
 
     def test_getdata(self):
         web = 'file:///' + os.path.join(os.path.dirname(os.path.abspath('test2.html')), 'test2.html')
-        self.assertEqual(getdata('anstest2.html', web), {'data_disease': '1',
+        self.assertEqual(getdata('anstest2.html', web.replace("\\", "/")), {'data_disease': '1',
             'data_healed': '2',
             'data_deathes': '3'})
 
     def test_getstat(self):
         web = 'file:///' + os.path.join(os.path.dirname(os.path.abspath('test3.html')), 'test3.html')
-        self.assertEqual(getstat('anstest3.html', web), {0: ['09.05.20', 90400, 12779, 1010],
-         1: ['10.05.20', 94882, 13790, 1068],
-         2: ['11.05.20', 96963, 17822, 1124],
-         3: ['12.05.20', 100480, 19642, 1179],
+        self.assertEqual(getstat('anstest3.html', web.replace("\\", "/")), {0: ['09.05.20', 90400, 12779, 1010],
+         1: ['10.05.20', 1, 1, 1],
+         2: ['11.05.20', 2, 2, 2],
+         3: ['12.05.20', 3, 3, 3],
          4: ['13.05.20', 103266, 21506, 1232],
          5: ['14.05.20', 106099, 23327, 1290],
          6: ['15.05.20', 109544, 24562, 1358],
